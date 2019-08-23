63d9ebb44878a7797d6d66523e80fc873513132d
Jonh ASem <45866310+letranquan2210@users.noreply.github.com>
2019-08-23 12:19:13 +0700

    Update StudentProgram.java

--- P0021_Student/StudentProgram.java
+++ P0021_Student/StudentProgram.java
@@ -1,34 +1,39 @@
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentProgram {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Student> studentList = new ArrayList<>();
         StudentManagement s = new StudentManagement();
         Scanner sc = new Scanner(System.in);
 
         Validator v = new Validator();
         while (true) {
             System.out.println("==========================");
-            s.displayStudent();
             displayMenu();
             int choice = v.checkchoice("Your Choice: ");
             switch (choice) {
                 case 1:
                     s.add();
+                    System.out.println("\n======================\n");
+                    s.displayStudent();
+                    System.out.println("\n======================\n");
                     break;
                 case 2:
                     System.out.println("Enter name to Search: ");
-                    String nameSearch = sc.nextLine();
+                    String nameSearch = v.checkString("");
                     s.search(nameSearch);
                     break;
                 case 3:
                     System.out.println("Enter ID to Update: ");
-                    String idUpdate = sc.nextLine();
-                    s.update(idUpdate);
+                    String idUpdate = v.checkString("");
+                    s.updateById(idUpdate);
                     break;
                 case 4:
-                    s.report();
+                    s.report2();
                     break;
                 case 5:
                     System.exit(0);
